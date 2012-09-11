c9ad63c889aaf853b88f19ce731fb094aa1e2f99
Ed Scott <eblakescott@gmail.com>
2012-09-11 16:27:19 -0500

    ran db migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120907220158) do
+ActiveRecord::Schema.define(:version => 20120911212232) do
+
+  create_table "locations", :force => true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "image_url"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "products", :force => true do |t|
     t.string   "title"
