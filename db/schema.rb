0ba441277b3a03d6b97cef98b7ca7dd2b038c8f5
Ed Scott <eblakescott@gmail.com>
2012-09-19 17:16:31 -0500

    ran rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,21 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120911212232) do
+ActiveRecord::Schema.define(:version => 20120919221417) do
 
   create_table "locations", :force => true do |t|
     t.string   "title"
     t.text     "description"
     t.string   "image_url"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  create_table "museums", :force => true do |t|
+    t.string   "title"
+    t.string   "image_url"
+    t.text     "description"
+    t.string   "wait_time"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
   end
