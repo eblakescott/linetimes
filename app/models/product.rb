d1e503b3362321978e091a7ec4bfc2357a607188
Ed Scott <eblakescott@gmail.com>
2012-09-20 11:11:17 -0500

    problems running rake test; deleted all tests in test/functional

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :title, :category
-  validates :title, :description, :image_url, presence: true
+  validates :title, :description, :image_url, :category, presence: true
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true, format: {
     with:
--- app/models/product.rb~
+++ app/models/product.rb~
@@ -6,5 +6,6 @@ class Product < ActiveRecord::Base
     with:
     %r{\.(gif|jpg|png)\Z}i,
     message: 'must be a URL for GIF, JPG, or PNG image.'
-}
+  }
+  has_many :waits
 end
